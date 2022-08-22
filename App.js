4fea41753b1d0de6874ff1dedd66fcb79c05621c
Joshua <joshua.hernandez.ph@gmail.com>
2022-08-22 10:00:53 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import Navbar from './components/Navbar';
+import Main from './components/Main';
+import './style.css';
+
+function App() {
+  return (
+    <div className="container">
+      <Navbar />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
